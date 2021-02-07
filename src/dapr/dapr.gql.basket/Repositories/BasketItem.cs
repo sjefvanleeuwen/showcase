5ae590c25c17b42fbb519bfa11ac72b42849628f
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-02-07 23:15:54 +0100

    xml comments for graphql annotations

--- src/dapr/dapr.gql.basket/Repositories/BasketItem.cs
+++ src/dapr/dapr.gql.basket/Repositories/BasketItem.cs
@@ -1,12 +1,35 @@
 namespace dapr.gql.basket.Repositories
 {
     /// <summary>
-    /// This is a basket item.
+    /// This is a basket item. Multiple items form the shopping basket for a customer.
     /// </summary>
-    /// <param name="Id">Unieuq Id of the basket item</param>
-    /// <param name="CustomerId">Customer Id this basket belongs to</param>
-    /// <param name="ProductId">The product Id in this basket item</param>
-    /// <param name="Quantity">The quantity selected</param>
-    /// <returns></returns>
-    public record BasketItem(int Id, int CustomerId, int ProductId, int Quantity);
+    public class BasketItem {
+        public BasketItem(int Id, int CustomerId, int ProductId, int Quantity){
+            this.Id = Id;
+            this.CustomerId = CustomerId;
+            this.ProductId = ProductId;
+            this.Quantity = Quantity;
+        }
+
+        /// <summary>
+        /// Unique Id of the basket item
+        /// </summary>
+        /// <value></value>
+        public int Id { get; }
+        /// <summary>
+        /// Customer to basket item relation
+        /// </summary>
+        /// <value></value>
+        public int CustomerId { get; }
+        /// <summary>
+        /// Product to basket item relation
+        /// </summary>
+        /// <value></value>
+        public int ProductId { get; }
+        /// <summary>
+        /// Quantity of the product in the basket item
+        /// </summary>
+        /// <value></value>
+        public int Quantity { get; }
+    } 
 }
