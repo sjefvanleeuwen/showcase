f48cb7890a114ea991b33b430a2ed092233f095c
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-02-08 11:22:33 +0100

    Adding documentation to Basket Commands

--- src/dapr/dapr.gql.basket/Repositories/Command.cs
+++ src/dapr/dapr.gql.basket/Repositories/Command.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using HotChocolate;
 
 namespace dapr.gql.basket.Repositories
@@ -8,12 +7,17 @@ namespace dapr.gql.basket.Repositories
     /// </summary>
     public class Command {
         /// <summary>
-        /// Gets all baskets
+        /// Update or insert a product item in the customers shopping basket
         /// </summary>
-        /// <param name="repository"></param>
+        /// <param name="repository">The repository where this item is stored</param>
+        /// <param name="item">The product item to upsert</param>
         /// <returns></returns>
-        public BasketItem UpsertItemToBasket(BasketItem item, [Service] BasketRepository repository) {
-            return repository.Add(item);
-        }
+        public BasketItem UpsertItemToBasket(BasketItem item, [Service] BasketRepository repository)=> repository.Add(item);
+        /// <summary>
+        /// Removes a product item from the customers shopping basket
+        /// </summary>
+        /// <param name="basketItemId">The unique identifier of the basket item</param>
+        /// <param name="repository">The repository where this item is stored</param>
+        public void RemoveItemFromBasket(int basketItemId, [Service] BasketRepository repository) => repository.Remove(basketItemId);
     }
 }
