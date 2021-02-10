27d94ad6cbd81594ac70d9541222051362d57c95
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-02-10 16:44:06 +0100

    Use interface in injection

--- src/dapr/dapr.gql.product/Startup.cs
+++ src/dapr/dapr.gql.product/Startup.cs
@@ -26,7 +26,7 @@ namespace dapr.gql.product
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "dapr.gql.product", Version = "v1" });
             });
             services
-                .AddSingleton<ProductRepositoryInMemory>()
+                .AddSingleton<IProductRepository, ProductRepositoryInMemory>()
                 .AddGraphQLServer()
                 .AddQueryType<Query>();
         }
