39f80c80394bee85a5f649bc0e661c3d5def074f
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-02-14 22:06:15 +0100

    rename and kubectl apply-all

---
+++ src/dapr/k8s/deployment/apply-all.bat
@@ -0,0 +1,5 @@
+kubectl apply -f .\dapr-gql-basket.yaml
+kubectl apply -f .\dapr-gql-customer.yaml
+kubectl apply -f .\dapr-gql-inventory.yaml
+kubectl apply -f .\dapr-gql-payment.yaml  
+kubectl apply -f .\dapr-gql-product.yaml 
