9eb10a5b8c9f9171c05b3ccb0dbe5ebb41f04502
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-02-16 23:12:24 +0100

    micro service unit tests project structure setup

---
+++ src/dapr/tests/dapr.gql.inventory.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dapr.gql.inventory.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/dapr/tests/dapr.gql.payment.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dapr.gql.payment.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
