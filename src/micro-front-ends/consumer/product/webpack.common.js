4e8bcaf842c18adddda34b69521309b131501a4c
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-02-28 12:13:02 +0100

    product micro front end build dev/prod/docker

---
+++ src/micro-front-ends/consumer/product/webpack.common.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  module: {
+    rules: [
+        {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+              options: {  
+                sourceMap: true,
+                modules: true,
+              }
+            }
+          ]
+        }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./src'), path.resolve('./node_modules')],
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        template: './public/index.html',
+    }),
+    new ModuleFederationPlugin({
+        name: 'product_card',
+        filename: 'remoteEntry.js',
+        exposes: {
+            './ProductCard': './src/ProductCard',
+        },
+        shared: ['react', 'react-dom'],
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
---
+++ src/micro-front-ends/consumer/product/webpack.dev.js
@@ -0,0 +1,12 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 3001,
+    contentBase: './dist',
+    open: true
+  },
+});
