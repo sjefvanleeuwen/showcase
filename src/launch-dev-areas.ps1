f9ff36239278358a09c4fb398a65e68d927c4829
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-03-01 12:16:19 +0100

    shorter ps annotation for dev areas menu

--- src/launch-dev-areas.ps1
+++ src/launch-dev-areas.ps1
@@ -1,17 +1,8 @@
 . ./utilities/cli/Write-Menu.ps1
 
-$menuReturn = Write-Menu -Title "Cowz n' Bullz Dev Areas" -Sort -MultiSelect -Entries "dapr","documentation","simulation","micro front ends (consumer)"
-
-foreach ($i in $menuReturn){
-    Write-Host 'launching area' $i
-    switch($i) {
-        # Micro services area
-        "dapr" {code ./dapr/}
-        # Documentation area
-        "documentation" {code ../}
-        # Simulation area
-        "simulation" {code ./simulation}
-        # Micro Front Ends area
-        "micro front ends (consumer)" {code ./micro-front-ends/consumer}
-    }
+$menuReturn = Write-Menu -Title "Cowz n' Bullz Dev Areas" -Sort -MultiSelect -Entries @{
+    "dapr" = "(code ./dapr/)"
+    "documentation" = "(code ../)"
+    "simulation" = "(code ./simulation)"
+    "micro front ends (consumer)" =  "(code ./micro-front-ends/consumer)"
 }
