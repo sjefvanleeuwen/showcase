a9dce919c781d05f092dcc971e26ecabcf5aa96c
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-03-05 22:23:58 +0100

    cdn add bulma

--- src/micro-front-ends/debug-consumer.ps1
+++ src/micro-front-ends/debug-consumer.ps1
@@ -1,3 +1,4 @@
 Start-Process -FilePath "npm" -ArgumentList "run dev:serve" -WorkingDirectory "./consumer/product"
 Start-Process -FilePath "npm" -ArgumentList "run dev:serve" -WorkingDirectory "./common/form-engine"
 Start-Process -FilePath "npm" -ArgumentList "run dev:serve" -WorkingDirectory "./consumer/portal"
+Start-Process -FilePath "npm" -ArgumentList "run start" -WorkingDirectory "./consumer/cdn"
